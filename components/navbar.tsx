9eeebf547484a38a73fd13524f7d16fb8fdce63d
Stone Werner <stonewerner@berkeley.edu>
2024-08-30 07:30:23 -0700

    added dialogue sheet back for small screens

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,7 +24,48 @@ export function NavBar() {
 
     return (
         <div className="flex items-center min-w-full w-full fixed justify-center p-2 z-[50] mt-[2rem]">
-            <div className="flex justify-between md:w-[720px] w-[95%] border dark:border-zinc-900 dark:bg-black bg-opacity-10 relative backdrop-filter backdrop-blur-lg bg-white border-white border-opacity-20 rounded-xl p-2 shadow-lg">
+            <div className="flex justify-between md:w-[90%] w-[95%] max-w-[1200px] border dark:border-zinc-900 dark:bg-black bg-opacity-10 relative backdrop-filter backdrop-blur-lg bg-white border-white border-opacity-20 rounded-xl p-2 shadow-lg">
+            <Dialog>
+                    <SheetTrigger className="min-[825px]:hidden p-2 transition">
+                        <MenuIcon />
+                    </SheetTrigger>
+                    <SheetContent side="left">
+                        <SheetHeader>
+                            <SheetTitle>fabrika.</SheetTitle>
+                            <SheetDescription>
+                                Scale and launch products with expert developers, on-demand, at a flat monthly fee
+                            </SheetDescription>
+                        </SheetHeader>
+                        <div className="flex flex-col space-y-3 mt-[1rem] z-[99]">
+                            <DialogClose asChild>
+                                <Link href="/">
+                                    <Button variant="outline" className="w-full">Home</Button>
+                                </Link>
+                            </DialogClose>
+                            <DialogClose asChild>
+                                <Link href="/software">
+                                    <Button variant="outline" className="w-full">Software</Button>
+                                </Link>
+                            </DialogClose>
+                            <DialogClose asChild>
+                                <Link href="/automation">
+                                    <Button variant="outline" className="w-full">Automation</Button>
+                                </Link>
+                            </DialogClose>
+                            <DialogClose asChild>
+                                <Link href="/blog">
+                                    <Button variant="outline" className="w-full">Blog</Button>
+                                </Link>
+                            </DialogClose>
+                            <DialogClose asChild>
+                                <Link href="/projects">
+                                    <Button variant="outline" className="w-full">Projects</Button>
+                                </Link>
+                            </DialogClose>
+                            
+                        </div>
+                    </SheetContent>
+                </Dialog>
                 <NavigationMenu>
                     <NavigationMenuList className="max-[825px]:hidden ">
                         <Link href="/" className="pl-2">
@@ -74,7 +115,6 @@ export function NavBar() {
                             <DropdownMenuItem>Glossary</DropdownMenuItem>
                         </DropdownMenuContent>
                     </DropdownMenu>
-                    {/*<ModeToggle />*/}
                 </div>
             </div>
         </div>
